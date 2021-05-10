b58fc2d0fd828c8349b1a6494f08eaecf8b5dbbb
Braulio Diez Botella <braulio.diez@gmail.com>
2021-05-10 20:04:20 +0200

    working pending study

--- 01-pinta-mapa/src/index.ts
+++ 01-pinta-mapa/src/index.ts
@@ -1,6 +1,6 @@
 import * as d3 from "d3";
 import * as topojson from "topojson-client";
-const mapatopojson = require("./maps/autonomous_regions.json");
+const mapatopojson = require("./maps/municipalities.json");
 
 const svgDimensions = { width: 1024, height: 768 };
 const margin = { left: 5, right: 5, top: 10, bottom: 10 };
@@ -17,22 +17,26 @@ const svg = d3
   .attr("style", "background-color: #FBFAF0");
 
 const aProjection = d3.geoMercator();
-const geoPath = d3.geoPath().projection(aProjection);
+// Let's make the map bigger to fit in our resolution
+//.scale(1600)
+// Let's center the map
+//.translate([500, 1400]);
 
+const geoPath = d3.geoPath(aProjection);
+
+/*
 const mapageojson = topojson.feature(
   mapatopojson,
-  mapatopojson.objects.autonomous_regions
-);
+  mapatopojson.objects.municipalities
+);*/
 
-aProjection.fitSize(
-  [chartDimensions.width, chartDimensions.height],
-  mapageojson
-);
+const mesh = topojson.mesh(mapatopojson);
+
+aProjection.fitSize([chartDimensions.width, chartDimensions.height], mesh);
 
 svg
-  .selectAll("path")
-  .data(mapageojson["features"])
-  .enter()
   .append("path")
-  .attr("class", "contorno")
-  .attr("d", geoPath as any);
+  .attr("d", geoPath(mesh))
+  .attr("fill", "none")
+  .attr("stroke", "black");
+// .attr("class", "contorno")
