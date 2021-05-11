c64732f7f2e34bb9e52079e7c542a5ebe71e8628
Braulio Diez Botella <braulio.diez@gmail.com>
2021-05-11 19:04:10 +0200

    two flavors

--- 01-pinta-mapa/src/index.ts
+++ 01-pinta-mapa/src/index.ts
@@ -1,6 +1,6 @@
 import * as d3 from "d3";
 import * as topojson from "topojson-client";
-const mapatopojson = require("./maps/municipalities.json");
+const mapatopojson = require("./maps/provinces.json");
 
 const svgDimensions = { width: 1024, height: 768 };
 const margin = { left: 5, right: 5, top: 10, bottom: 10 };
@@ -24,19 +24,46 @@ const aProjection = d3.geoMercator();
 
 const geoPath = d3.geoPath(aProjection);
 
-/*
 const mapageojson = topojson.feature(
   mapatopojson,
-  mapatopojson.objects.municipalities
-);*/
+  mapatopojson.objects.provinces
+);
 
+aProjection.fitSize(
+  [chartDimensions.width, chartDimensions.height],
+  mapageojson
+);
+
+svg
+  .selectAll("path")
+  .data(mapageojson["features"])
+  .enter()
+  .append("path")
+  .attr("class", "contorno")
+  .attr("d", geoPath as any)
+  .on("click", (m, d: any) => {
+    alert(d.properties.name);
+  });
+
+/*
+// convert to geojson
 const mesh = topojson.mesh(mapatopojson);
 
 aProjection.fitSize([chartDimensions.width, chartDimensions.height], mesh);
 
+// Mirar esto: https://bl.ocks.org/mbostock/4060606
 svg
   .append("path")
   .attr("d", geoPath(mesh))
-  .attr("fill", "none")
-  .attr("stroke", "black");
+  //.attr("fill", "none")
+  .attr("stroke", "black")
+  .style("fill", function (d: any) {
+    console.log(d);
+    return "none";
+  })
+  .on("mouseover", function (d, i) {
+    console.log(d);
+    d3.select(this).attr("class", "selected-country");
+  });
 // .attr("class", "contorno")
+*/
